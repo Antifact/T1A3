d34b2e48359e4c03c53731bb83e751b899333b0f
Antifact <soulstormxiii@hotmail.com>
2021-12-20 02:16:33 +1000

    added more items to store.

--- src/classes/Shop.rb
+++ src/classes/Shop.rb
@@ -6,22 +6,32 @@ class Shop
     def initialize
         @items = {
             "fruit & veg" => [
-                ShopItem.new("Apple", 0.99),
-                ShopItem.new("Banana", 1.49),
-                ShopItem.new("Orange", 0.79)
+                ShopItem.new("Apple", 0.68),
+                ShopItem.new("Banana", 0.63),
+                ShopItem.new("Strawberries", 2.50),
+                ShopItem.new("Watermelon", 7.70),
+                ShopItem.new("Pineapple", 1.75),
+                ShopItem.new("Orange", 1.95),
+                ShopItem.new("Carrots", 1.00),
+                ShopItem.new("Tomato", 1.09),
+                ShopItem.new("Lettuce", 1.80)
             ],
             "meat" => [
-                ShopItem.new("Chicken", 4.99),
-                ShopItem.new("Beef", 19.99),
-                ShopItem.new("Pork", 14.99)
+                ShopItem.new("Chicken 2.25kg", 14.63),
+                ShopItem.new("Beef Mince 500g", 9.00),
+                ShopItem.new("Sausage 1.8kg", 11.00),
+                ShopItem.new("Roast Pork Shoulder", 19.50)
             ],
             "bakery" => [
                 ShopItem.new("Bread", 3.49),
-                ShopItem.new("Donut", 1.99)
+                ShopItem.new("Anzac Biscuits", 6.00),
+                ShopItem.new("Donuts 12pk", 3.00)
             ],
             "dairy" => [
-                ShopItem.new("Milk 3L", 3.99),
-                ShopItem.new("Cheese", 4.99)
+                ShopItem.new("Milk 3L", 3.90),
+                ShopItem.new("Butter", 4.50),
+                ShopItem.new("Eggs", 4.50),
+                ShopItem.new("Tasty Cheese", 5.30)
             ],
             "snacks" => [
                 ShopItem.new("Chips", 3.00),
@@ -34,7 +44,9 @@ class Shop
             ],
             "drinks" => [
                 ShopItem.new("Cola 1.25L", 3.99),
-                ShopItem.new("Lemonade 1.25L", 3.99)
+                ShopItem.new("Lemonade 1.25L", 3.99),
+                ShopItem.new("Blue V", 4.00),
+                ShopItem.new("Water 6 x 1.5L", 4.75)
             ]
         }
     end
